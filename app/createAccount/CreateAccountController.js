85f295b98ef38fd0e917fa3bd86a9d7275e13243
Tex <danotex1128@gmail.com>
2016-05-14 09:37:47 -0400

    Abstract validation back to service and return result

--- app/createAccount/CreateAccountController.js
+++ app/createAccount/CreateAccountController.js
@@ -14,7 +14,6 @@
 
         //functions
         vm.login = login;
-        //vm.creatUserObj = creatUserObj;
         vm.fieldValidate = fieldValidate;
         vm.check = check;
         vm.checkPass = checkPass;
@@ -24,16 +23,16 @@
             fName: false,
             lName: false,
             eMail: false,
-            pass1: false,
-            pass2: false
+            first: false,
+            second: false
         };
         
         vm.valid = {
             fName: false,
             lName: false,
             email: false,
-            pass1: false,
-            pass2: false
+            first: false,
+            second: false
         };
 
         function fieldValidate(fieldData) {
@@ -44,28 +43,13 @@
         function check(variable, name) {
             if (!variable)
                 return;
-                
-            switch (name) {
-                case 'lName':
-                case 'fName':
-                    if (variable.length >= 4) {
-                        vm.valid[name] = true;
-                        return 1;
-                    } else {
-                        vm.valid[name] = false;
-                        return 2;
-                    }
-                    
-                case 'email':
-                    var re = /\S+@\S+\.\S+/;
-                    if(re.test(variable)) {
-                        vm.valid[name] = true;
-                        return 1;
-                    } else {
-                        vm.valid[name] = false;
-                        return 2;
-                    }            
-            }    
+            var valid = validationService.check(variable, name)
+            if (valid == 1){
+                vm.valid[name] = true;
+            } else {
+                vm.valid[name] = false;
+            }
+            return valid;
         }
         
         function hadFocus(name) {
@@ -75,66 +59,14 @@
         function checkPass(name) {
             if (!vm.focus.pass1 && !vm.focus.pass2 && !vm.user.first && !vm.user.second)
                 return;
-
-            //function validatePassword() {
-                if(vm.user.first === undefined || vm.user.second === undefined)
-                    return;
-
-                else if (vm.user.first.length <= 10) {
-                    //user.first.setCustomValidity("10 characters min");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else if (vm.user.first.length >= 101) {
-                    //user.first.setCustomValidity("100 characters max");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else if (!/[a-z]/.test(vm.user.first)) {
-                    //user.first.setCustomValidity("1 lowercase letter required");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else if (!/[A-Z]/.test(vm.user.first)) {
-                    //user.first.setCustomValidity("1 upper case letter required");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else if (!/\d/.test(vm.user.first)) {
-                    //user.first.setCustomValidity("number required");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else if (/[\(\)\[\]\{\}\<\>\.\,\`\~\?\"\;\:\\\|\+\=\-\_\/\s\t\r]{1,}/.test(vm.first)) {
-                    //user.first.setCustomValidity("illegal password");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else if (/[\!\@\#\$\%\^\&\*]/.test(vm.user.first) == false) {
-                    //user.first.setCustomValidity("symbol required");
-                    vm.valid[name] = false;
-                    return 2;
-                }
-
-                else {
-                    if (vm.user.first != vm.user.second) {
-                    //vm.user.first.setCustomValidity("Passwords Do Not Match");
-                        vm.valid[name] = false;
-                        return 2;
-                    }
-                    //vm.user.first.setCustomValidity("");
-                    //vm.user.second.setCustomValidity("");
-                    //vm.user.first.checkValidity("");
-                    //vm.user.second.checkValidity("");
-                    vm.valid[name] = true;
-                    return 1;
-                }
+            
+            var valid = validationService.checkPass(vm.user.first, vm.user.second);
+            if (valid == 1){
+                vm.valid[name] = true;
+            } else {
+                vm.valid[name] = false;
+            }
+            return valid; 
         }
 
         function login() {
@@ -145,7 +77,6 @@
                 
             }
             window.location.href = "#/viewEvents";
-        }
-        
+        }  
     }
 })();
--- app/services/validationService.js
+++ app/services/validationService.js
@@ -5,11 +5,97 @@
 
     function validationService() {
         var service = {
-            validatePassword: validatePassword
+            validatePassword: validatePassword,
+            check: check,
+            checkPass: checkPass
         };
 
         return service;
 
-        
+
+        function check(variable, name) {
+            if (!variable)
+                return;
+
+            switch (name) {
+                case 'lName':
+                case 'fName':
+                    if (variable.length >= 4) {
+                        return 1;
+                    } else {
+                        return 2;
+                    }
+
+                case 'email':
+                    var re = /\S+@\S+\.\S+/;
+                    if (re.test(variable)) {
+                        return 1;
+                    } else {
+                        return 2;
+                    }
+            }
+        }
+
+        function checkPass(pass1, pass2) {
+            //function validatePassword() {
+            if (pass1 === undefined || pass2 === undefined)
+                return;
+
+            else if (pass1.length <= 10) {
+                //user.first.setCustomValidity("10 characters min");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else if (pass1.length >= 30) {
+                //user.first.setCustomValidity("100 characters max");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else if (!/[a-z]/.test(pass1)) {
+                //user.first.setCustomValidity("1 lowercase letter required");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else if (!/[A-Z]/.test(pass1)) {
+                //user.first.setCustomValidity("1 upper case letter required");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else if (!/\d/.test(pass1)) {
+                //user.first.setCustomValidity("number required");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else if (/[\(\)\[\]\{\}\<\>\.\,\`\~\?\"\;\:\\\|\+\=\-\_\/\s\t\r]{1,}/.test(pass1)) {
+                //user.first.setCustomValidity("illegal password");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else if (/[\!\@\#\$\%\^\&\*]/.test(pass1) == false) {
+                //user.first.setCustomValidity("symbol required");
+                //vm.valid[name] = false;
+                return 2;
+            }
+
+            else {
+                if (pass1 != pass2) {
+                    //vm.user.first.setCustomValidity("Passwords Do Not Match");
+                    //vm.valid[name] = false;
+                    return 2;
+                }
+                //vm.user.first.setCustomValidity("");
+                //vm.user.second.setCustomValidity("");
+                //vm.user.first.checkValidity("");
+                //vm.user.second.checkValidity("");
+                //vm.valid[name] = true;
+                return 1;
+            }
+        }
     }
 } ())
